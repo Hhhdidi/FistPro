6771e0e1d826bc5f55e2309a20af8fb5100fbde4
Hhhdidi <1822659168@qq.com>
2023-02-20 20:58:43 +0800

    Initial commit

---
+++ src/Hello.java
@@ -0,0 +1,10 @@
+/**
+ * @author ChenJingwei
+ * @date 2023/2/20 20:37
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello java");
+        System.out.println("hello git and github");
+    }
+}
