b24e6703195adb8d28e2051ff650d8b2d4e3afe0
Hhhdidi <1822659168@qq.com>
2023-02-21 10:57:04 +0800

    commit test 02

--- src/Hello.java
+++ src/Hello.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+
 /**
  * @author ChenJingwei
  * @date 2023/2/20 20:37
@@ -8,5 +11,18 @@ public class Hello {
         System.out.println("hello git and github");
         System.out.println("do some changes");
         System.out.println("im cjw");
+
+        System.out.println("===========================");
+
+        ArrayList<Cat> arrayList = new ArrayList<Cat>();
+        arrayList.add(new Cat("kitty", 4));
+        arrayList.add(new Cat("wendi", 5));
+
+        Iterator<Cat> iter = arrayList.iterator();
+        while (iter.hasNext()) {
+            System.out.println(iter.next());;
+
+        }
+
     }
 }
